b4185b0d325a7ab47b67b692721156f039735d15
Светлана Плотникова <uzer1874@rambler.ru>
2017-10-25 14:18:10 +0400

    Update index.php

--- users/v/index.php
+++ users/v/index.php
@@ -2,35 +2,30 @@
 
     <h1>Список пользователей</h1>
     <ul>
-		
-		<? foreach ($users as $user): ?>
-		    
-			
-				<div class="num" >
-					<?echo $user['id_user']?>
-				</div>
-				<div class="blok">
-				    <a href="./index.php?act=user&&id=<?echo $user['id_user']?>"><?echo $user['name']?></a>
-				</div>
-				<div class="blok">
-				    Пол - <?$gender=($user['gender']=='j')?'ж':'м'; echo $gender;?>
-				</div>
-				<div class="blok">
-				    Дата рождения: <?$mass=explode('-',$user['date']);
-			        $mass=array_reverse($mass);
-			        $date=implode('-',$mass);
-			        echo $date;?>
+	<? foreach ($users as $user): ?>
+	     <div class="num" >
+		 <?echo $user['id_user']?>
+	      </div>
+	      <div class="blok">
+	           <a href="./index.php?act=user&id=<?echo $user['id_user']?>"><?echo $user['name']?></a>
+	      </div>
+	      <div class="blok">
+		   Пол - <?$gender=($user['gender']=='j')?'ж':'м'; echo $gender;?>
+	      </div>
+	      <div class="blok">
+		   Дата рождения: <?$mass=explode('-',$user['date']);
+		   $mass=array_reverse($mass);
+		   $date=implode('-',$mass);
+		   echo $date;?>
 				</div>
 				<div class="blok">Номер телефона:<?echo $user['phone']?></div>
-				<a href="./index.php?act=edit&&id=<?echo $user['id_user']?>" class="edit" alt="редактировать"></a>
+				<a href="./index.php?act=edit&id=<?echo $user['id_user']?>" class="edit" alt="редактировать"></a>
 				<a href="" class='del delete' alt="удалить" id="<?echo "a".$user['id_user']?>"></a>
-				<br>
-                <br>			
-			
-		<? endforeach ?>
-		
+	     			
+	<? endforeach ?>		
     </ul>
-
+    <br>
+    <br>
     <a href="index.php?act=new" id="reg" class="button">Добавить запись</a>
 
 			
