a7abf2e932eeac249f16cfa2bab2f4d27979fb12
Светлана Плотникова <uzer1874@rambler.ru>
2017-10-25 14:57:11 +0400

    Update C_Base.php

--- users/c/C_Base.php
+++ users/c/C_Base.php
@@ -1,1 +1,33 @@
-<?php//// Базовый контроллер сайта.//include_once('C_Controller.php');abstract class C_Base extends C_Controller{	protected $title;		// заголовок страницы	protected $content;		// содержание страницы	//	// Конструктор.	//	function __construct()	{			}		protected function before()	{		$this->title = 'Название сайта';		$this->content = '';		include_once('./m/startup.php');	    startup();		header('Content-type: text/html; charset=utf-8');	}		//	// Генерация базового шаблонаы	//		public function render()	{		$vars = array('title' => $this->title, 'content' => $this->content);			$page = $this->Template('v/sabl.php', $vars);						echo $page;	}	}
+<?php
+//
+// Базовый контроллер сайта.
+//
+include_once('C_Controller.php');
+abstract class C_Base extends C_Controller
+{
+	protected $title;		// заголовок страницы
+	protected $content;		// содержание страницы
+	//
+	// Конструктор.
+	//
+	function __construct()
+	{		
+	}
+	protected function before()
+	{
+		$this->title = 'Название сайта';
+		$this->content = '';
+		include_once('./m/startup.php');
+	    startup();
+		header('Content-type: text/html; charset=utf-8');
+	}
+	//
+	// Генерация базового шаблонаы
+	//	
+	public function render()
+	{
+		$vars = array('title' => $this->title, 'content' => $this->content);	
+		$page = $this->Template('v/sabl.php', $vars);				
+		echo $page;
+	}	
+}
